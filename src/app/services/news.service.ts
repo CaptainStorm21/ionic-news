78356c57b34574f4c48ca590c50bff4b1b8f33cd
Captain Storm <CaptainStorm21@users.noreply.github.com>
2019-12-30 00:43:08 -0800

    spinner
    spinner loading is set up / see top news

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 // api hooks
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpParams} from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
 
 const apiKey = environment.apiKey;
 const apiUrl = environment.apiUrl;
@@ -14,9 +15,18 @@ const params = new HttpParams().set('apiKey', apiKey);
 })
 export class NewsService {
 
-  constructor(private http: HttpClient ) { }
+  constructor(private http: HttpClient, public loadingController:  LoadingController  ) { }
+
+  async showLoading(){
+    const loading = await this.loadingController.create({
+      duration: 3000
+    });
+
+    return await loading.present();
+  }
 
   getData(url){
+    this.showLoading();
     // return this.http.get(`${apiUrl}/${url}apiKey=${apiKey}`);
     return this.http.get(`${apiUrl}/${url}`, { params });
   }
